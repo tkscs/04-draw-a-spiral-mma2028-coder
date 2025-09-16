9685ebb0fba76734a370dd9886b16b12c09d891e
Matt <mma2028@kehillahstudent.org>
2025-09-16 10:26:31 -0700

    asdf

--- 02_rectangular_golden_spiral.py
+++ 02_rectangular_golden_spiral.py
@@ -12,7 +12,21 @@ initial_arm_length * (phi**i)
 """
 
 ### YOUR CODE STARTS HERE
-
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(230)
 
 ### YOUR CODE ENDS HERE
 
